dd24e5aa6da980e58f9afddc20654b99d242bd54
marcus pedersen <marcus@mpedersen.me>
2024-07-30 14:40:27 +0200

    Adding to the media player

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { IMedia } from "../interfaces";
 
+import "./MediaPlayer.css"
 
 interface ICurrentSongProps {
     song: IMedia;
@@ -9,7 +10,23 @@ interface ICurrentSongProps {
 export function MediaPlayer({ song }: ICurrentSongProps): ReactElement {
     return (
         <div className="currentSong">
-            Current Song
+            <img src="{song.image}" alt="{song.title}" />
+            <span className="material-symbols-outlined"> Add </span>
+            <div>
+                <p>{song.title}</p>
+                <p>{song.artist}</p>
+            </div>
+            <span className="material-symbols-outlined"> Favorite </span>
+            <div className="timeline">
+
+            </div>
+            <div className="playControls">
+                <span className="material-symbols-outlined"> Repeat </span>
+                <span className="material-symbols-outlined"> skip_previous </span>
+                <span className="material-symbols-outlined"> play_circle </span>
+                <span className="material-symbols-outlined"> skip_next </span>
+                <span className="material-symbols-outlined"> Shuffle </span>
+            </div>
         </div>
     )
 }
